991d512ec54e510815eef70ba521e804017d4873
stuartdaviddean <165738870+stuartdaviddean@users.noreply.github.com>
2025-04-18 16:10:17 +1000

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,8 +1,8 @@
 <?php
 	//database connection details
 	$host = 'localhost';
-	$user = 'root';
-	$password = '';
+	$user = 'stuart';
+	$password = 'VerySecretPassword!';
 	$database = 'gorgeous_cupcakes';
 
 	//connect to database with a try/catch statement
